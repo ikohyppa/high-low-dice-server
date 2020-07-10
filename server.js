bd5ac0d50d2277351e45a418a1bed85e17a4ad58
ikohyppa <iko.hyppanen@gmail.com>
2020-07-10 19:57:21 +0300

    error handling

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var _ = require('lodash');
 
 const cors = require('cors');
 var uuidv4 = require('uuid').v4;
@@ -17,29 +18,74 @@ app.get('/room', function (req, res, next) {
     id: uuidv4()
   };
   const user = req.query.user;
-  rooms[room.id] = room;
-  playerlist[room.id] = [];
-  playerlist[room.id].push(user);
-  const response = {
-    id: rooms[room.id].id,
-    name: rooms[room.id].name,
-    user: user,
-    playerlist: playerlist[room.id]
-  };
-  res.json(response);
+  const roomNames = _.map(rooms, room => room.name);
+  if (_.includes(roomNames, room.name)) {
+    const response = {
+      status: 'error',
+      data: { roomName: room.name },
+      message: '',
+      error: `Room name '${room.name}' already in use!`
+    };
+    res.json(response);
+  } else {
+    rooms[room.id] = room;
+    playerlist[room.id] = [];
+    playerlist[room.id].push(user);
+    const response = {
+      status: 'ok',
+      data: {
+        id: rooms[room.id].id,
+        name: rooms[room.id].name,
+        user: user,
+        playerlist: playerlist[room.id]
+      },
+      message: 'Room created successfully!',
+      error: null
+    };
+    res.json(response);
+  }
 });
 
 app.get('/room/:roomId', function (req, res, next) {
   const roomId = req.params.roomId;
   const user = req.query.user;
-  playerlist[roomId].push(user);
-  const response = {
-    id: rooms[roomId].id,
-    name: rooms[roomId].name,
-    user: user,
-    playerlist: playerlist[roomId]
-  };
-  res.json(response);
+  console.log(roomId);
+  console.log(user);
+  const roomIds = _.map(rooms, room => room.id);
+  console.log(roomIds)
+  if (!_.includes(roomIds, roomId)) {
+    const response = {
+      status: 'error',
+      data: { roomId: roomId },
+      message: '',
+      error: `There is no room with  ID '${roomId}'`
+    };
+    res.json(response);
+  } else if (_.includes(playerlist[roomId], user)) {
+    const response = {
+      status: 'error',
+      data: { userName: user },
+      message: '',
+      error: `Player named '${user}' is already in the room!`
+    };
+    res.json(response);
+  } else {
+    playerlist[roomId].push(user);
+    console.log(rooms);
+    console.log(playerlist);
+    const response = {
+      status: 'ok',
+      data: {
+        id: rooms[roomId].id,
+        name: rooms[roomId].name,
+        user: user,
+        playerlist: playerlist[roomId]
+      },
+      message: 'Room joined successfully!',
+      error: null
+    };
+    res.json(response);
+  }
 });
 
 const rolldice = () => {
